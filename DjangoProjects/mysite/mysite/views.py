411128713d7dd3fcb13ae51804e225ea5d6631bd
subhadip2000 <subhadipnandi14@gmail.com>
2020-08-31 21:22:16 +0530

    commited IRcell

---
+++ DjangoProjects/mysite/mysite/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def start(request):
+   return render(request,'index.html')
+def amantripathi(request):
+   return render(request, 'amantripathi.html')
+def snehajais(request):
+   return render(request, 'snehajais.html')
+def sudesnabose(request):
+   return render(request, 'sudesnabose.html')
+def jaiprakash(request):
+   return render(request, 'jaiprakash.html')
+def rohitverma(request):
+   return render(request, 'rohitverma.html')
+def manishpandey(request):
+   return render(request, 'manishpandey.html')
+def saketjain(request):
+   return render(request, 'saketjain.html')
+def rehansharma(request):
+   return render(request, 'rehansharma.html')
+def mohitaggarwal(request):
+   return render(request, 'mohitaggarwal.html')
